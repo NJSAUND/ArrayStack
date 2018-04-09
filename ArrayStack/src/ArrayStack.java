2eecc7e135a1a31d61f7c0396f80a11a37a96eaa
NJSAUND <njsaund@gmail.com>
2018-04-08 22:28:47 -0700

    final

--- ArrayStack/src/ArrayStack.java
+++ ArrayStack/src/ArrayStack.java
@@ -3,43 +3,53 @@ import java.util.*;
 
 public class ArrayStack {
 	
-	int maxSize;
-	private double stack[];
-	int top;
+	int maxSize;//allows variable to be used across class
+	private double stack[]; //initiates array
+	int top; //tracks the top of the array
 	
 	public ArrayStack(int size) {
 		
-		maxSize = size;
-		stack = new double[maxSize];
-		top = -1 ;
+		maxSize = size; //allows variable to be used across class
+		stack = new double[maxSize]; //sets array size to int size
+		top = -1 ; //starts at index -1 as 0, since index 0 is the first one
 	}
 	
-	public void push(double item) {
-		if(top == maxSize -1) {
-			
-		System.out.println("Stack Overflow!");
-		return;
+	public void push(double item) throws Exception {//push method to push double item onto stack
+		if(top == maxSize) {
+			throw new Exception("Stack overflow!");//error handling if more than maxSize
 		}			
 		else {
-			stack[++top] = item;
+			stack[++top] = item; //adds to stack
 		}
 	}
 	
-	public double pop(){
-		return stack[top--];
+	public double pop() throws Exception {//pop method 
+		if(top == -1) {//if top is outside of array displays error
+				throw new Exception("Stack is empty: cannot pop!");
+		}
 		
+		return top--;
+			
 	}
 	
-	public boolean isEmpty() {
-		return (top == -1);	
+	public boolean isEmpty() {//isEmpty method
+		return top == -1;	//returns false and true depending on if its empty
 	}
 		
-	public double peek(int n) {
-		return stack[n];
+	public double peek(int n) throws Exception{
+		if (n > top) {//if n is less than top throw error
+			throw new Exception("Desired input has not be initialized yet!"); //Only allows the inputted numbers to be peeked. Won't peek at a spot that hasn't been pushed into
+		}
+		else if(top > -1) {//if top is greater than -1 it will continue as norml
+			return stack[n];
+		}
+		else {//throws error for exceeding boundary or size
+			throw new Exception("Desired input is outside of array!");
+		}
 	}
 	
-	public int count() {
-		return maxSize;
+	public int count() { //count method
+		return top+1; //the top is at the current index, so by adding one to account for index 0 we have the total inputs.
 	}
 
 }
--- ArrayStack/src/TestArrayStack.java
+++ ArrayStack/src/TestArrayStack.java
@@ -1,29 +1,37 @@
-
 public class TestArrayStack {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		ArrayStack myStack = new ArrayStack(1);
+		ArrayStack myStack = new ArrayStack(5); // initiates new array from ArrayStack
 		
-
+		System.out.println(myStack.isEmpty());//will return true: is empty
 		myStack.push(1);
-		System.out.println(myStack.peek(0));
+		System.out.println(myStack.peek(0));//peeks at first index
+		System.out.println(myStack.isEmpty());
+		System.out.println("Count: "+ myStack.count());//shows total used indices
+		myStack.pop();//pop out top index
+		System.out.println("Count: "+ myStack.count());//shows total used indices
+		myStack.push(2);//pushes n to array
+		System.out.println(myStack.peek(0));//peeks at first index
+		myStack.push(3);//pushes n to array
+		System.out.println(myStack.peek(1));//peeks at second index
+		myStack.push(4);//pushes n to array
+		System.out.println(myStack.peek(2));//peeks at third index
+		myStack.push(5);//pushes n to array
+		System.out.println(myStack.peek(3));//peeks at fourth index
+		myStack.push(6);//pushes n to array
+		System.out.println(myStack.peek(4));//peeks at fifth index
+				
+		myStack.pop();//pop out top index
 		
-		System.out.println(myStack.pop());
-		
-		myStack.push(2);
-		System.out.println(myStack.peek(0));
-		//myStack.push(3);
-		//System.out.println(myStack.peek(3));
-		
-		//System.out.println(myStack.count());
-		
-		//myStack.push(4);
+		System.out.println();
+		System.out.println(myStack.peek(myStack.top));//shows that it is LIFO
+		System.out.println();
+		myStack.push(7);//pushes n to array
+		System.out.println(myStack.peek(4));//peeks at fifth index
+		System.out.println("Count: "+ myStack.count());//shows total used indices
 
-
-		
-		
-		
+		System.out.println("\nSUCCESS");
 		
 	}
 
